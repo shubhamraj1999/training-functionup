1174df8e54c4b05ddddb493665436e2e205d6b31
Aniket Kumar <aniketkumar@Anikets-MacBook-Pro.local>
2022-08-10 13:24:49 +0530

    players api

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,49 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players =
+  [
+    {
+      "name": "manish",
+      "dob": "1/1/1995",
+      "gender": "male",
+      "city": "jalandhar",
+      "sports": [
+        "swimming"
+      ]
+    },
+    {
+      "name": "gopal",
+      "dob": "1/09/1995",
+      "gender": "male",
+      "city": "delhi",
+      "sports": [
+        "soccer"
+      ],
+    },
+    {
+      "name": "lokesh",
+      "dob": "1/1/1990",
+      "gender": "male",
+      "city": "mumbai",
+      "sports": [
+        "soccer"
+      ],
+    },
+  ]
+
+
+
+router.post('/players', function (req, res) {
+
+  
+  let newplayer = req.body.player
+  players.push(newplayer)
+  res.send({ data: players, status: true })
+})
+
+
+
+
+
 module.exports = router;
